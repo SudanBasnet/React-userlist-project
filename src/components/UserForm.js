61dec644f9ebaef9db76be8faa3f90ad165e2ef2
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-27 19:10:09 +1000

    form created

---
+++ src/components/UserForm.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const UserForm = () => {
+  return (
+    <div className="w-50 shadow-lg p-5 m-auto mt-5 rounded">
+      <form action="">
+        <div class="row">
+          <div class="col-3">
+            <select
+              name="gender"
+              class="form-select"
+              id="autoSizingSelect"
+              required
+            >
+              <option selected>Choose...</option>
+              <option value="m">Male</option>
+              <option value="f">Female</option>
+            </select>
+          </div>
+          <div class="col-6">
+            <input
+              name="name"
+              type="text"
+              class="form-control"
+              placeholder="User name"
+              aria-label="User name"
+              required
+            />
+          </div>
+          <div className="col-3 d-grid">
+            <button className="btn btn-primary">Add User</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default UserForm;
