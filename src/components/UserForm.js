7792da38d13c447822d77682472cfd256149f2d6
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-27 19:28:14 +1000

    building usertable and CSS

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -2,32 +2,33 @@ import React from "react";
 
 const UserForm = () => {
   return (
-    <div className="w-50 shadow-lg p-5 m-auto mt-5 rounded">
+    <div className="w-50 shadow-lg p-3 m-auto mt-5 rounded">
       <form action="">
-        <div class="row">
-          <div class="col-3">
+        <div className="row g-2">
+          <div className="col-md-3">
             <select
               name="gender"
-              class="form-select"
+              className="form-select"
               id="autoSizingSelect"
               required
+              defaultValue=""
             >
               <option selected>Choose...</option>
               <option value="m">Male</option>
               <option value="f">Female</option>
             </select>
           </div>
-          <div class="col-6">
+          <div className="col-md-6">
             <input
               name="name"
               type="text"
-              class="form-control"
+              className="form-control"
               placeholder="User name"
               aria-label="User name"
               required
             />
           </div>
-          <div className="col-3 d-grid">
+          <div className="col-md-3 d-grid">
             <button className="btn btn-primary">Add User</button>
           </div>
         </div>
